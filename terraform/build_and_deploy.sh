b670341c6ee3d0b9a57518f19576fc4032720f95
Tristan Garwood <tgarwood@indigoag.com>
2019-05-19 14:49:43 -0400

    adding shippable, terraform

--- terraform/build_and_deploy.sh
+++ terraform/build_and_deploy.sh
@@ -3,6 +3,9 @@
 cd ..
 printf '\n\nStarting the Terraforming!\n\n'
 cd infra
+
 terraform plan -out=plan.out
+
 terraform apply plan.out
+
 terraform output -json | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]"
