dbb89288119d3f9ed2365097bb959fd61ab53734
Tristan Garwood <tgarwood@indigoag.com>
2019-05-19 21:16:43 -0400

    adding shippable, terraform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-val ktorVersion = "1.1.5"
+val ktorVersion = "1.2.0"
 
 repositories {
     google()
@@ -22,7 +22,7 @@ dependencies {
 
     fun ktor(s: String = "", v: String = ktorVersion) = "io.ktor:ktor$s:$v"
     compile(ktor())
-    compile(ktor("-gson", "1.2.0"))
+    compile(ktor("-gson"))
     compile(ktor("-html-builder"))
     compile(ktor("-server-netty"))
     compile("ch.qos.logback:logback-classic:1.2.1")
